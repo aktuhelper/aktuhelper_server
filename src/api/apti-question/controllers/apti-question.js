df8d3fa90c288cd7c77da4211a084fd500d68a44
Khushbu Singh <singhkhushbu3200@gmail.com>
2026-02-03 12:42:06 +0530

    Fff

---
+++ src/api/apti-question/controllers/apti-question.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * apti-question controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::apti-question.apti-question');
---
+++ src/api/practice-set/routes/practice-set.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * practice-set router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::practice-set.practice-set');
